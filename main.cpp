2549955b3db31d121785e1c67da4c043aad224a6
dovydasgre <dovydasgrebliunas@gmail.com>
2023-11-14 22:05:37 +0200

    Added private members.

--- main.cpp
+++ main.cpp
@@ -66,44 +66,123 @@ string unixTimeToHumanReadable(time_t timestamp) {
 }
 
 class User {
-public:
+private:
     string name;
     string public_key;
     double balance;
 
+public:
     User() : name(""), public_key(""), balance(0.0) {}
 
     User(const string& name, const string& public_key, double balance)
         : name(name), public_key(public_key), balance(balance) {}
+
+    const string& getName() const {
+        return name;
+    }
+
+    void setName(const string& newName) {
+        name = newName;
+    }
+
+    const string& getPublicKey() const {
+        return public_key;
+    }
+
+    void setPublicKey(const string& newPublicKey) {
+        public_key = newPublicKey;
+    }
+
+    double getBalance() const {
+        return balance;
+    }
+
+    void setBalance(double newBalance) {
+        balance = newBalance;
+    }
 };
 
 class Transaction {
-public:
-    string transactionId = "";
+private:
+    string transactionId;
     string sender;
     string receiver;
     double sum;
 
+public:
     Transaction() : transactionId(""), sender(""), receiver(""), sum(0.0) {}
 
     Transaction(const string& transactionId, const string& sender, const string& receiver, double sum)
         : transactionId(transactionId), sender(sender), receiver(receiver), sum(sum) {}
+
+    const string& getTransactionId() const {
+        return transactionId;
+    }
+
+    void setTransactionId(const string& newTransactionId) {
+        transactionId = newTransactionId;
+    }
+
+    const string& getSender() const {
+        return sender;
+    }
+
+    void setSender(const string& newSender) {
+        sender = newSender;
+    }
+
+    const string& getReceiver() const {
+        return receiver;
+    }
+
+    void setReceiver(const string& newReceiver) {
+        receiver = newReceiver;
+    }
+
+    double getSum() const {
+        return sum;
+    }
+
+    void setSum(double newSum) {
+        sum = newSum;
+    }
 };
 
 class Block {
-public:
+private:
     string hash;
     vector<Transaction> transactions;
 
+public:
     Block() : hash("") {}
 
     void addTransactions(const vector<Transaction>& newTransactions) {
         transactions.insert(transactions.end(), newTransactions.begin(), newTransactions.end());
     }
+
+    const string& getHash() const {
+        return hash;
+    }
+
+    void setHash(const string& newHash) {
+        hash = newHash;
+    }
+
+    const vector<Transaction>& getTransactions() const {
+        return transactions;
+    }
+
+    void setTransactions(const vector<Transaction>& newTransactions) {
+        transactions = newTransactions;
+    }
+
+    void addTransaction(const Transaction& newTransaction) {
+        transactions.push_back(newTransaction);
+    }
 };
 
 class Blockchain {
-public:
+private:
     string prevHash;
     int timestamp;
     string version;
@@ -112,32 +191,108 @@ public:
     string diff;
     Block block;
 
-    void initialize(const string& prevHash, int timestamp, const string& version, const string& merkelRoot, int nonce, const string& diff) {
-        this->prevHash = prevHash;
-        this->timestamp = timestamp;
-        this->version = version;
-        this->merkelRoot = merkelRoot;
-        this->nonce = nonce;
-        this->diff = diff;
+public:
+    void setPrevHash(const string& newPrevHash) {
+        prevHash = newPrevHash;
     }
+
+    const string& getPrevHash() const {
+        return prevHash;
+    }
+
+    void setTimestamp(int newTimestamp) {
+        timestamp = newTimestamp;
+    }
+
+    int getTimestamp() const {
+        return timestamp;
+    }
+
+    void setVersion(const string& newVersion) {
+        version = newVersion;
+    }
+
+    const string& getVersion() const {
+        return version;
+    }
+
+    void setMerkelRoot(const string& newMerkelRoot) {
+        merkelRoot = newMerkelRoot;
+    }
+
+    const string& getMerkelRoot() const {
+        return merkelRoot;
+    }
+
+    void setNonce(int newNonce) {
+        nonce = newNonce;
+    }
+
+    int getNonce() const {
+        return nonce;
+    }
+
+    void setDiff(const string& newDiff) {
+        diff = newDiff;
+    }
+
+    const string& getDiff() const {
+        return diff;
+    }
+
+    const Block& getBlock() const {
+        return block;
+    }
+
+    void setBlock(const Block& newBlock) {
+        block = newBlock;
+    }
+
+    void setBlockHash(const string& newHash) {
+        block.setHash(newHash);
+    }
+
+    const string& getBlockHash() const {
+        return block.getHash();
+    }
+
+    const vector<Transaction>& getBlockTransactions() const {
+        return block.getTransactions();
+    }
+
+    void addBlockTransaction(const Transaction& newTransaction) {
+        block.addTransaction(newTransaction);
+    }
+
 };
 
-void readUsers(vector<User> &con) {
+void readUsers(vector<User>& con) {
     ifstream df("Users.txt");
     if (df.is_open()) {
+        string name, publickey;
+        double balance;
         User u;
-        while (df >> u.name >> u.public_key >> u.balance) {
+        while (df >> name >> publickey >> balance) {
+            u.setName(name);
+            u.setPublicKey(publickey);
+            u.setBalance(balance);
             con.push_back(u);
         }
         df.close();
     }
 }
 
-void readTrans(vector<Transaction> &con) {
+void readTrans(vector<Transaction>& con) {
     ifstream df("transactions.txt");
     if (df.is_open()) {
+        string sender, receiver, id;
+        double sum;
         Transaction t;
-        while (df >> t.sum >> t.sender >> t.receiver >> t.transactionId) {
+        while (df >> sum >> sender >> receiver >> id) {
+            t.setSum(sum);
+            t.setReceiver(receiver);
+            t.setSender(sender);
+            t.setTransactionId(id);
             con.push_back(t);
         }
         df.close();
@@ -147,15 +302,15 @@ void readTrans(vector<Transaction> &con) {
 bool verifyTransaction(const Transaction& trans, const vector<User>& users) {
     User sender, receiver;
     for (const User& user : users) {
-        if (user.public_key == trans.sender) {
+        if (user.getPublicKey() == trans.getSender()) {
             sender = user;
         }
-        if (user.public_key == trans.receiver) {
+        if (user.getPublicKey() == trans.getReceiver()) {
             receiver = user;
         }
     }
 
-    if (sender.balance >= trans.sum) {
+    if (sender.getBalance() >= trans.getSum()) {
         return true;
     }
 
@@ -177,7 +332,7 @@ string generateMerkleRoot(vector<Transaction> trans) {
   string word, hash;
 
   for(unsigned int i = 0; i < trans.size(); i++) {
-    merkel.push_back(trans.at(i).transactionId);
+    merkel.push_back(trans.at(i).getTransactionId());
   }
 
   while (merkel.size() > 1) {
@@ -214,53 +369,44 @@ string mineBlock(Blockchain &bc, string prevHash, int b) {
     int nonce = threadNum;
 
     if (b == 0) {
-        bc.prevHash = customHash("");
+        bc.setPrevHash(customHash(""));
     } else {
-        bc.prevHash = prevHash;
+        bc.setPrevHash(prevHash);
     }
 
 
-    string target = bc.diff;
+    string target = bc.getDiff();
 
     while (newHash.substr(0, 3) != target) { 
-        newHash = customHash(bc.diff + bc.merkelRoot + bc.prevHash + to_string(bc.timestamp) + bc.version + to_string(nonce));
+        newHash = customHash(bc.getDiff() + bc.getMerkelRoot() + bc.getPrevHash() + to_string(bc.getTimestamp()) + bc.getVersion() + to_string(nonce));
 
         nonce += threadCount;
     }
 
-    bc.block.hash = newHash;
-    bc.timestamp = time(nullptr);
-    bc.nonce = nonce;
+    bc.setBlockHash(newHash);
+    bc.setTimestamp(time(nullptr));
+    bc.setNonce(nonce);
     return newHash;
 }
 
 void printBlock(Blockchain bc, int nr) {
   cout << "------------------------- #" << nr+1 << " BLOCK -------------------------" << endl;
-  cout << "Block hash: " << bc.block.hash << endl;
-  cout << "Previous block hash: " << bc.prevHash << endl;
-  cout << "Version: " << bc.version << endl;
-  cout << "Merkle Root: " << bc.merkelRoot.substr(0, 64) << endl;
-  cout << "Nonce: " << bc.nonce << endl;
-  cout << "Mined on: " << unixTimeToHumanReadable(bc.timestamp) << " (GMT+2)" << endl;
-  cout << "Difficulty: " << bc.diff << endl;
+  cout << "Block hash: " << bc.getBlockHash() << endl;
+  cout << "Previous block hash: " << bc.getPrevHash() << endl;
+  cout << "Version: " << bc.getVersion() << endl;
+  cout << "Merkle Root: " << bc.getMerkelRoot().substr(0, 64) << endl;
+  cout << "Nonce: " << bc.getNonce() << endl;
+  cout << "Mined on: " << unixTimeToHumanReadable(bc.getTimestamp()) << " (GMT+2)" << endl;
+  cout << "Difficulty: " << bc.getDiff() << endl;
   cout << "----------------------------------------------------------" << endl;
-}
-
-void printTrans(Transaction t) {
-  cout << "---------------------- TRANSACTION ----------------------" << endl;
-  cout << "ID: " << t.transactionId << endl;
-  cout << "Sender: " << t.sender << endl;
-  cout << "Receiver: " << t.receiver << endl;
-  cout << "Amount: " << t.sum << endl;
-  cout << "---------------------------------------------------------" << endl;
 }
 
 void usersData(const vector<User>& users) {
     ofstream wf("UsersRez.txt");
     for (const User& user : users) {
-        wf << user.name << endl;
-        wf << user.public_key << endl;
-        wf << fixed << setprecision(2) << user.balance << endl;
+        wf << user.getName() << endl;
+        wf << user.getPublicKey() << endl;
+        wf << fixed << setprecision(2) << user.getBalance() << endl;
         wf << endl;
     }
     wf.close();
@@ -284,12 +430,12 @@ int main() {
   for (int i = 0; i < 5; i++) {
         stop = 0;
         Blockchain newBC;
-        while (newBC.block.transactions.size() != 100 && !trans.empty()) {
+        while (newBC.getBlockTransactions().size() != 100 && !trans.empty()) {
             uniform_int_distribution<> distr(0, trans.size() - 1);
             int random = distr(gen);
 
             if (verifyTransaction(trans[random], users)) {
-                newBC.block.transactions.push_back(trans[random]);
+                newBC.addBlockTransaction(trans[random]);
             }
 
             trans.erase(trans.begin() + random);
@@ -306,42 +452,42 @@ while (mined != 5) {
     {
         int nonce;
         string temp;
-        bc[mined].version = "v" + to_string(mined + 1) + ".0";
-        bc[mined].diff = "000";
-        bc[mined].merkelRoot = generateMerkleRoot(bc[mined].block.transactions);
+        bc[mined].setVersion("v" + to_string(mined + 1) + ".0");
+        bc[mined].setDiff("000");
+        bc[mined].setMerkelRoot(generateMerkleRoot(bc[mined].getBlockTransactions()));
 
         nonce = limit * omp_get_thread_num();
 
-        temp = mineBlock(bc[mined], (mined == 0) ? "" : bc[mined - 1].block.hash, mined);
+        temp = mineBlock(bc[mined], (mined == 0) ? "" : bc[mined - 1].getBlockHash(), mined);
         if (temp.size() > 2) {
             #pragma omp critical
             {
                 if (who == -1) {
                     who = omp_get_thread_num(); 
-                    bc[mined].block.hash = temp;
+                    bc[mined].setBlockHash(temp);
                     stop = true;
-                    bc[mined].nonce = nonce;
+                    bc[mined].setNonce(nonce);
                 }
             }
         }
     }
 
-    if (bc[mined].block.hash.length() > 1 && who != -1) {
+    if (bc[mined].getBlockHash().length() > 1 && who != -1) {
           cout << "MINED BY " << who << " THREAD" << endl;
-            for (auto tran : bc[mined].block.transactions) {
+            for (auto tran : bc[mined].getBlockTransactions()) {
                 int send = 0, get = 0;
                 for (int i = 0; i < 1000; i++) {
-                    if (users[i].public_key == tran.sender)
+                    if (users[i].getPublicKey() == tran.getSender())
                         send = i;
-                    else if (users[i].public_key == tran.receiver)
+                    else if (users[i].getPublicKey() == tran.getReceiver())
                         get = i;
                     if (send != 0 && get != 0)
                         break;
                 }
 
-                if (users[send].balance >= tran.sum) {
-                    users[send].balance -= tran.sum;
-                    users[get].balance += tran.sum;
+                if (users[send].getBalance() >= tran.getSum()) {
+                    users[send].setBalance(users[send].getBalance()-tran.getSum());
+                    users[get].setBalance(users[send].getBalance()+tran.getSum());
                 }
             }
             printBlock(bc[mined], mined);
